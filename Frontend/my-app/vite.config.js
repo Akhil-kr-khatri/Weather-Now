d4ccf1f34eb0d2f8d4fa5f41324d6f145a8bcd32
Akhil-DatwLabs <akhil.khatri@datwalabs.com>
2025-09-05 09:54:42 +0530

    Updating Packages

--- Frontend/my-app/vite.config.js
+++ Frontend/my-app/vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'weather-now-frontend-98hf.onrender.com' 
+    ],
+    port: 3000, 
+  }
 })
